efeddfaad21f9f1e9b7770b6bcb70d52c65e5501
Piotr Matuszak <56255498+TheHund1@users.noreply.github.com>
2026-09-16 12:19:22 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 1.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "what"
+print(type(variable))
+variable = []
+print(type(variable))
